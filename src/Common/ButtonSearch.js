98d56b5acfce67bf3a7f6ebbdbba5e51ffef237b
Ruslan Isaev <smog47@gmail.com>
2018-02-23 07:10:43 +0300

    fix button

--- src/Common/ButtonSearch.js
+++ src/Common/ButtonSearch.js
@@ -1,6 +1,5 @@
 import React from "react";
 import styled from "styled-components";
-import { Link } from "react-router-dom";
 import aero from "./aero.svg";
 
 const Search = styled.div`
@@ -11,12 +10,13 @@ const Search = styled.div`
 `;
 
 const Img = styled.img`
-  margin-left: 16px;
+  margin-left: 23px;
 `;
 
 const Button = styled.button`
   border-radius: 4px;
-  font-size: 25px;
+  font-size: 28px;
+  line-height: 33px;
   font-weight: 900;
   background: #ff9241;
   border: none;
@@ -25,19 +25,26 @@ const Button = styled.button`
   width: 100%;
   box-sizing: border-box;
   cursor: pointer;
+  display: flex;
+  justify-content: center;
+  align-items: center;
   &:hover,
   &:focus {
     background: #fca15f;
+  }
+  @media (min-width: 768px) {
+    flex-basis: 50%;
+  }
+  @media (min-width: 1200px) {
+    flex-basis: 33%;
   }
 `;
 export default function() {
   return (
     <Search>
-      <Link to="/search">
-        <Button>
-          Найти билеты <Img alt="Самолет" src={aero} />
-        </Button>
-      </Link>
+      <Button>
+        Найти билеты <Img alt="Самолет" src={aero} />
+      </Button>
     </Search>
   );
 }
